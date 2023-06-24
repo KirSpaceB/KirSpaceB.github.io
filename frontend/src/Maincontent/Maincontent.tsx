33e38cca77604e3f25b2f875114478050b0b8dbb
KirSpaceB <productivekirk@gmail.com>
2023-06-23 17:35:32 -0700

    feat:main content is finished

--- frontend/src/Maincontent/Maincontent.tsx
+++ frontend/src/Maincontent/Maincontent.tsx
@@ -40,8 +40,30 @@ export const Maincontent = () => {
                   </ul>
                 </div>
               </div>
+              <h1 className="text-4xl font-semibold text-center text-gray-200 dark:text-gray-600 ml-auto mr-auto mb-10 sm:mb-16 flex gap-2 items-center justify-center flex-grow">
+                ChatGPT
+                <span className="bg-yellow-200 text-yellow-900 py-0.5 px-1.5 text-xs md:text-sm rounded-md uppercase">Plus</span>
+              </h1>
             </div>
           </div>
+          <form className="stretch mx-2 flex flex-row gap-3 last:mb-2 md:mx-4 md:last:mb-6 lg:mx-auto lg:max-w-2xl xl:max-w-3xl" action="">
+            <div className="relative flex h-full flex-1 items-stretch md:flex-col">
+              <div className="flex flex-row bg-gray-700 w-[650px] rounded-lg">
+                <textarea className="m-0 w-full resize-none border-0 bg-transparent p-0 pr-10 focus:ring-0 focus-visible:ring-0 dark:bg-transparent md:pr-12 pl-3 md:pl-0">
+                </textarea>
+                <button className=" p-1 rounded-md md:bottom-3 md:p-2 md:right-3 dark:hover:bg-gray-900 dark:disabled:hover:bg-transparent right-2 disabled:text-gray-400 enabled:bg-brand-purple text-white bottom-1.5 transition-colors disabled:opacity-40">
+                  <span>
+                  <svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 16 16" stroke-linecap="round" stroke-linejoin="round" className="h-4 w-4 transition-colors text-brand-green" height="16" width="16" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M.5 1.163A1 1 0 0 1 1.97.28l12.868 6.837a1 1 0 0 1 0 1.766L1.969 15.72A1 1 0 0 1 .5 14.836V10.33a1 1 0 0 1 .816-.983L8.5 8 1.316 6.653A1 1 0 0 1 .5 5.67V1.163Z" fill="gray" stroke="gray"></path>
+                  </svg>
+                  </span>
+                </button>
+              </div>
+              <div className="px-3 pb-3 pt-2 text-center text-xs text-gray-600 dark:text-gray-300 md:px-4 md:pb-6 md:pt-3">
+                <span>ChatGPT may produce inaccurate information about people, places, or facts. ChatGPT May 24 Version</span>
+              </div>
+            </div>
+          </form>
         </main>
       </div>
     </div>
