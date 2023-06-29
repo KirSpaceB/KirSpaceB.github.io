f8ba549dabf7b8ea8cb2d6fdf1254d7d89949c4a
KirSpaceB <productivekirk@gmail.com>
2023-06-29 15:32:54 -0700

    fix: Reminder Linux is case sensitive

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,11 @@
-import { Maincontent } from "./MainContent/Maincontent";
 import { Sidebar } from "./Sidebar/Sidebar";
+import { MainContent } from "./MainContent/MainContent";
 
 const App = () => {
   return (
     <div className="overflow-hidden w-screen h-screen relative flex z-0">
       <Sidebar/>
-      <Maincontent/>
+      <MainContent/>
     </div>
   )
 }
--- frontend/src/Maincontent/Maincontent.tsx
+++ frontend/src/Maincontent/Maincontent.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { Answer } from "../Answer/Answer";
-export const Maincontent = () => {
+
+export const MainContent = () => {
   const [text,setText] = useState("");
   // When this is set to true we render a new component
   const [addToMainContext, setAddToMainContext] = useState(false)
