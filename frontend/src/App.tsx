7abcb4c6ae0c5dff01ac8555d7b0df0a474905f1
KirSpaceB <productivekirk@gmail.com>
2023-06-29 15:24:45 -0700

    fix: Added Fixes to the Dockerfile

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,8 +2,6 @@ import { Maincontent } from "./MainContent/Maincontent";
 import { Sidebar } from "./Sidebar/Sidebar";
 
 const App = () => {
-
-
   return (
     <div className="overflow-hidden w-screen h-screen relative flex z-0">
       <Sidebar/>
