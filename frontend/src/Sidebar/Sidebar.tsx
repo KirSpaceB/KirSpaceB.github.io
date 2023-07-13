6340d7ef3c49608e4e2e656b7871492d02de498b
KirSpaceB <productivekirk@gmail.com>
2023-07-13 15:46:50 -0700

    fix: added temp fix for mobile responsiveness for sidebar

--- frontend/src/Sidebar/Sidebar.tsx
+++ frontend/src/Sidebar/Sidebar.tsx
@@ -1,19 +1,20 @@
 import SideBarProfile from "../Sidebar/SideBarImages/Sidebarprofile.png"
 import Resume from "../../public/Resume.pdf"
+import { PlusSign } from "./svg/PlusSign";
 export const Sidebar = () => {
 
 
   return (
-    <div className="dark flex-shrink-0 bg-[#252323]">
+    <div className="hidden sm:flex flex-shrink-0 bg-[#252323]">
       <div className="h-full w-[260px]">
         <div className="flex h-full min-h-0 flex-col">
           <nav className="flex h-full w-full flex-col p-2" aria-label="Chat History">
             <div className="mb-1 flex flex-row gap-2">
-              <a href="" className="flex p-3 items-center gap-3 transition-colors duration-200 text-white cursor-pointer text-sm rounded-md border border-white/20 hover:bg-gray-500/10 h-11 flex-shrink-0 flex-grow ">
-                <svg stroke="currentColor" fill="none" strokeWidth="2" viewBox="0 0 24 24" strokeLinecap="round" strokeLinejoin="round" className="h-4 w-4" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
-                  <line x1="12" y1="5" x2="12" y2="19"></line>
-                  <line x1="5" y1="12" x2="19" y2="12"></line>
-                </svg>
+              <a 
+                href="" 
+                className="flex p-3 items-center gap-3 transition-colors duration-200 text-white cursor-pointer text-sm rounded-md border border-white/20 hover:bg-gray-500/10 h-11 flex-shrink-0 flex-grow"
+              >
+                <PlusSign/>
                 New Chat
               </a>
               <span>
