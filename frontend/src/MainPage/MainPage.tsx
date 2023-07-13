21c66a3325d4be3e2b6d025526a02f57866c2f3b
KirSpaceB <productivekirk@gmail.com>
2023-07-13 15:28:39 -0700

    feat:abstracted svgs in mainpage

--- frontend/src/MainPage/MainPage.tsx
+++ frontend/src/MainPage/MainPage.tsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from "react";
 import { Answer } from "../Answer/Answer";
+import { GPT3Logo } from "./svg/GPT3Logo";
+import { GPT4Logo } from "./svg/GPT4Logo";
 
 export const MainPage = () => {
   const [text,setText] = useState("");
@@ -20,7 +22,7 @@ export const MainPage = () => {
         // Add the next character from the message
         return prevText + message.charAt(prevText.length);
       });
-    }, 50); // adjust the speed of typing here
+    }, 35); // adjust the speed of typing here
     return () => clearInterval(timer);
   }, []);
   useEffect(() => {
@@ -39,11 +41,10 @@ export const MainPage = () => {
                     <ul className="flex flex-row w-full list-none gap-1 sm:w-auto">
                       <li className="group/toggle w-full">
                         <button className="w-full cursor-pointer">
-                          <div className="group/button relative flex w-full items-center justify-center gap-1 rounded-lg border py-3 outline-none transition-opacity duration-100 sm:w-auto sm:min-w-[148px] md:gap-2 md:py-2.5 border-black/10 bg-white text-gray-900 shadow-[0_1px_7px_0px_rgba(0,0,0,0.06)] hover:!opacity-100 dark:border-[#4E4F60] dark:bg-gray-700 dark:text-gray-100">
+                          <div
+                           className=" flex w-full items-center justify-center gap-1 rounded-lg border py-3 outline-none sm:min-w-[148px] md:gap-2 md:py-2.5 border-black/10 bg-white text-gray-900 shadow-[0_1px_7px_0px_rgba(0,0,0,0.06)] hover:!opacity-100 dark:border-[#4E4F60] dark:bg-gray-700 dark:text-gray-100">
                             <span className="relative max-[370px]:hidden">
-                              <svg stroke="currentColor" fill="none" strokeWidth="2" viewBox="0 0 16 16" strokeLinecap="round" strokeLinejoin="round" className="h-4 w-4 transition-colors text-brand-green" height="16" width="16" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M9.586 1.526A.6.6 0 0 0 8.553 1l-6.8 7.6a.6.6 0 0 0 .447 1h5.258l-1.044 4.874A.6.6 0 0 0 7.447 15l6.8-7.6a.6.6 0 0 0-.447-1H8.542l1.044-4.874Z" fill="green" stroke="green"></path>
-                              </svg>
+                              <GPT3Logo/>
                             </span>
                             <span className="truncate text-sm font-medium md:pr-1.5 pr-1.5">
                               GPT-3.5 :D
@@ -53,11 +54,10 @@ export const MainPage = () => {
                       </li>
                       <li className="group/toggle w-full">
                         <button className="w-full cursor-pointer">
-                          <div className="group/button relative flex w-full items-center justify-center gap-1 rounded-lg border py-3 outline-none transition-opacity duration-100 sm:w-auto sm:min-w-[148px] md:gap-2 md:py-2.5 border-black/10 bg-white text-gray-900 shadow-[0_1px_7px_0px_rgba(0,0,0,0.06)] hover:!opacity-100 dark:bg-gray-900 dark:text-gray-100">
+                          <div className="group/button relative flex w-full items-center justify-center gap-1 rounded-lg border py-3 outline-none transition-opacity duration-100 sm:w-auto sm:min-w-[148px] md:gap-2 md:py-2.5 border-black/10 bg-white text-gray-900 shadow-[0_1px_7px_0px_rgba(0,0,0,0.06)] hover:!opacity-100 dark:bg-gray-900 dark:text-gray-100"
+                          >
                             <span className="relative max-[370px]:hidden">
-                              <svg stroke="currentColor" fill="none" strokeWidth="2" viewBox="0 0 16 16" strokeLinecap="round" strokeLinejoin="round" className="h-4 w-4 transition-colors text-brand-green" height="16" width="16" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M12.784 1.442a.8.8 0 0 0-1.569 0l-.191.953a.8.8 0 0 1-.628.628l-.953.19a.8.8 0 0 0 0 1.57l.953.19a.8.8 0 0 1 .628.629l.19.953a.8.8 0 0 0 1.57 0l.19-.953a.8.8 0 0 1 .629-.628l.953-.19a.8.8 0 0 0 0-1.57l-.953-.19a.8.8 0 0 1-.628-.629l-.19-.953h-.002ZM5.559 4.546a.8.8 0 0 0-1.519 0l-.546 1.64a.8.8 0 0 1-.507.507l-1.64.546a.8.8 0 0 0 0 1.519l1.64.547a.8.8 0 0 1 .507.505l.546 1.641a.8.8 0 0 0 1.519 0l.546-1.64a.8.8 0 0 1 .506-.507l1.641-.546a.8.8 0 0 0 0-1.519l-1.64-.546a.8.8 0 0 1-.507-.506L5.56 4.546Zm5.6 6.4a.8.8 0 0 0-1.519 0l-.147.44a.8.8 0 0 1-.505.507l-.441.146a.8.8 0 0 0 0 1.519l.44.146a.8.8 0 0 1 .507.506l.146.441a.8.8 0 0 0 1.519 0l.147-.44a.8.8 0 0 1 .506-.507l.44-.146a.8.8 0 0 0 0-1.519l-.44-.147a.8.8 0 0 1-.507-.505l-.146-.441Z" fill="purple" stroke="purple"></path>
-                              </svg>
+                              <GPT4Logo/>
                             </span>
                             <span className="truncate text-sm font-medium md:pr-1.5 pr-1.5">
                               GPT 4.0 :D
@@ -69,7 +69,6 @@ export const MainPage = () => {
                   </div>
                 }
               </div>
-              
               {!addToMainContext &&
                 <h1 className="text-4xl font-semibold text-center text-gray-200 dark:text-gray-600 ml-auto mr-auto mb-10 sm:mb-16 flex gap-2 items-center justify-center flex-grow">
                   ChatGPT
@@ -77,7 +76,6 @@ export const MainPage = () => {
                 </h1>
               }
               {addToMainContext ? <Answer/> : null }
-
             </div>
           </div>
           <form className="stretch mx-2 flex flex-row gap-3 last:mb-2 md:mx-4 md:last:mb-6 lg:mx-auto lg:max-w-2xl xl:max-w-3xl" action="">
---
+++ frontend/src/MainPage/svg/GPT3Logo.tsx
@@ -0,0 +1,10 @@
+export const GPT3Logo = () => {
+  return (
+    <>
+      <svg stroke="currentColor" fill="none" strokeWidth="2" viewBox="0 0 16 16" strokeLinecap="round" strokeLinejoin="round" className="h-4 w-4 transition-colors text-brand-green" height="16" width="16" xmlns="http://www.w3.org/2000/svg">
+        <path d="M9.586 1.526A.6.6 0 0 0 8.553 1l-6.8 7.6a.6.6 0 0 0 .447 1h5.258l-1.044 4.874A.6.6 0 0 0 7.447 15l6.8-7.6a.6.6 0 0 0-.447-1H8.542l1.044-4.874Z" fill="green" stroke="green">
+        </path>
+      </svg>
+    </>
+  )
+}
