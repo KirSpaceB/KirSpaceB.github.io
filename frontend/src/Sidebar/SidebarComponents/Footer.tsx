7a65ea5bb80acd07625323f2f3071d6a426c8d95
Kirk Flores <113055345+KirSpaceB@users.noreply.github.com>
2023-07-21 07:47:02 -0700

    Refactor svg (#21)
    * refactor: created abstraction for svg
    
    * refactor: abstracted svgs to separate component

--- frontend/src/Sidebar/SidebarComponents/Footer.tsx
+++ frontend/src/Sidebar/SidebarComponents/Footer.tsx
@@ -1,15 +1,14 @@
+import { SvgComponent } from '../../shared/SvgComponent'
 import SideBarProfile from '../SideBarImages/Sidebarprofile.png'
+import { ThreeDots } from '../svg/ThreeDots'
 export const Footer = () => {
   return (
     <div className="border-t border-white/20 pt-2 empty:hidden">
       <div className="flex w-full items-center gap-2.5 rounded-md px-3 py-3 text-sm transition-colors duration-200 hover:bg-gray-800 group-ui-open:bg-gray-800">
         <img src={SideBarProfile} alt="Not working" className="w-[22px] relative" />
         <div className="text-white text-[12px] relative left-0">John Doe</div>
-        <svg stroke="currentColor" fill="none" strokeWidth="2" viewBox="0 0 24 24" strokeLinecap="round" strokeLinejoin="round" className="h-4 w-4 flex-shrink-0 text-gray-500 relative left-[80px]" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
-          <circle cx="12" cy="12" r="1"></circle>
-          <circle cx="19" cy="12" r="1"></circle>
-          <circle cx="5" cy="12" r="1"></circle>
-        </svg>
+        <SvgComponent icon={ThreeDots}/>
+
       </div>
     </div>
   )
--- frontend/src/Sidebar/SidebarComponents/NewChat.tsx
+++ frontend/src/Sidebar/SidebarComponents/NewChat.tsx
@@ -1,3 +1,5 @@
+import { SvgComponent } from "../../shared/SvgComponent"
+import { CloseSideBar } from "../svg/CloseSideBar"
 import { PlusSign } from "../svg/PlusSign"
 export const NewChat = () => {
   return (
@@ -6,16 +8,12 @@ export const NewChat = () => {
         href="" 
         className="flex p-3 items-center gap-3 transition-colors duration-200 text-white cursor-pointer text-sm rounded-md border border-white/20 hover:bg-gray-500/10 h-11 flex-shrink-0 flex-grow"
       >
-        <PlusSign/>
+        <SvgComponent icon={PlusSign}/>
         New Chat
       </a>
       <span>
         <a className="flex p-3 gap-3 transition-colors duration-200 text-white cursor-pointer text-sm rounded-md border border-white/20 hover:bg-gray-500/10 h-11 flex-shrink-0 items-center justify-center bg-[#252323] w-full">
-          <svg stroke="currentColor" fill="none" strokeWidth="2" viewBox="0 0 24 24" strokeLinecap="round" strokeLinejoin="round" className="h-4 w-4" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
-            <rect x="3" y="3" width="18" height={"18"} rx={"2"} ry={"2"}>
-            </rect>
-            <line x1="9" y1="3" x2="9" y2="21"></line>
-          </svg>
+          <SvgComponent icon={CloseSideBar}/>
         </a>
       </span>
     </div>
