b043e36279a2d55b489d1fa59d825f8c51ebd73b
KirSpaceB <productivekirk@gmail.com>
2023-07-23 17:08:53 -0700

    feat: added animations to the modal

--- frontend/src/WebDesignLandingPage/ButtonModal.tsx
+++ frontend/src/WebDesignLandingPage/ButtonModal.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './styles/Modal.css';
 
 type ButtonModalProps = {
@@ -7,11 +7,12 @@ type ButtonModalProps = {
 
 export const ButtonModal = ({onClose}: ButtonModalProps) => {
   const modalRef = useRef<HTMLDivElement>(null);
+  const [closing, setClosing] = useState(false);
 
   useEffect(() => {
     function handleClickOutside(event: MouseEvent) {
       if (modalRef.current && !modalRef.current.contains(event.target as Node)) {
-        onClose();
+        handleClose();
       }
     }
 
@@ -23,11 +24,16 @@ export const ButtonModal = ({onClose}: ButtonModalProps) => {
     };
   }, [onClose]);
 
+  const handleClose = () => {
+    setClosing(true);
+    setTimeout(onClose, 300);  // delay the onClose call for 300ms
+  };
+
   return (
     <>
       <div id='modal_overlay'></div>
-      <div id='modal_container' ref={modalRef}>
-        <button id='modal_close_button' onClick={onClose}>Close Modal</button>
+      <div id='modal_container' className={closing ? 'closing' : ''} ref={modalRef}>
+        <button id='modal_close_button' onClick={handleClose}>Close Modal</button>
       </div>
     </>
   )
